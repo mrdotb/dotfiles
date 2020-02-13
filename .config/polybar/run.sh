b895828cdf72b71400bd95e624364e8cbd8c041d
mrdotb <mrdotb@protonmail.com>
2020-02-13 15:48:22 +0700

    update polybar

--- .config/polybar/run.sh
+++ .config/polybar/run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-while ! `pgrep -x Xorg > /dev/null`
-do
-  sleep 1
-done
 
-while pgrep -u $UID -x polybar > /dev/null
-do
-  kill -9 `pgrep -u $UID -x polybar`
-  sleep 1;
-done
+killall -q polybar
 
-MONITOR=eDP-1-1 polybar top
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+MONITOR=eDP-1-1 polybar top &>> /home/mrdotb/.config/polybar/polylog &
+MONITOR=eDP-1-1 polybar bottom &>> /home/mrdotb/.config/polybar/polylog &
