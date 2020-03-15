2912cafc55926df9c514d55723a4e3274a2371bd
mrdotb <mrdotb@protonmail.com>
2020-03-15 14:50:57 +0700

    tune run.sh again

--- .config/polybar/run.sh
+++ .config/polybar/run.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
 config="/home/mrdotb/.config/polybar/config"
-monitor=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+monitor=$(ppolybar -m|tail -1|sed -e 's/:.*$//g'olybar -m|tail -1|sed -e 's/:.*$//g')
 
 # The CPU temperature zone change across reboot
 ## Get the zone
 zone=$(for i in /sys/class/hwmon/hwmon*/temp*_input;do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $(readlink -f $i)";done|gawk '{ if ($2 =="CPU") { print $3 } }')
-## Replace the zone in polybar config
+## Replace the temperature zone in polybar config
 sed -i -e "s@hwmon-path.*@hwmon-path = $zone@g" $config
 
-MONITOR=eDP-1 polybar top >>/tmp/polybar1.log 2>&1 &
-MONITOR=eDP-1 polybar bottom >>/tmp/polybar1.log 2>&1 &
-killall -q 9 polybar
-while pgrep -u $UID -x polybar > /dev/null
-do
-  killall -q 9 polybar
-  sleep 0.1
-done
+# launch and kill to prevent a bug at computer boot
+MONITOR=$monitor polybar top >>/tmp/polybar1.log 2>&1 &
+MONITOR=$monitor polybar bottom >>/tmp/polybar1.log 2>&1 &
 
-MONITOR=eDP-1 polybar top >>/tmp/polybar1.log 2>&1 &
-MONITOR=eDP-1 polybar bottom >>/tmp/polybar1.log 2>&1 &
+# sleep before kill
+sleep 0.1
+
+# Terminate already running bar instances
+killall -q 9 polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# launch again
+MONITOR=$monitor polybar top >>/tmp/polybar1.log 2>&1 &
+MONITOR=$monitor polybar bottom >>/tmp/polybar1.log 2>&1 &
