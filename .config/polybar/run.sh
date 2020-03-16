77dc19fa094e17e915104acecb9bf048f72ee7fd
mrdotb <mrdotb@protonmail.com>
2020-03-16 13:14:34 +0700

    use pkill instead of killall

--- .config/polybar/run.sh
+++ .config/polybar/run.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
 config="/home/mrdotb/.config/polybar/config"
-monitor=$(ppolybar -m|tail -1|sed -e 's/:.*$//g'olybar -m|tail -1|sed -e 's/:.*$//g')
+monitor="eDP-1"
+logfile="/tmp/polybar1.log"
 
-# The CPU temperature zone change across reboot
-## Get the zone
-zone=$(for i in /sys/class/hwmon/hwmon*/temp*_input;do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $(readlink -f $i)";done|gawk '{ if ($2 =="CPU") { print $3 } }')
-## Replace the temperature zone in polybar config
-sed -i -e "s@hwmon-path.*@hwmon-path = $zone@g" $config
+# if $logfile is not here then it's boot time
+if [ ! -f $logfile ]; then
+  # The CPU temperature zone change across reboot
+  ## Get the zone
+  zone=$(for i in /sys/class/hwmon/hwmon*/temp*_input;do echo "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*})) $(readlink -f $i)";done|gawk '{ if ($2 =="CPU") { print $3 } }')
 
-# launch and kill to prevent a bug at computer boot
-MONITOR=$monitor polybar top >>/tmp/polybar1.log 2>&1 &
-MONITOR=$monitor polybar bottom >>/tmp/polybar1.log 2>&1 &
+  ## Replace the temperature zone in polybar config
+  sed -i -e "s@hwmon-path.*@hwmon-path = $zone@g" $config
 
-# sleep before kill
-sleep 0.1
+  ## Launch 1st time
+  MONITOR=$monitor polybar top >> $logfile 2>&1 &
+  MONITOR=$monitor polybar bottom >> $logfile 2>&1 &
+fi
 
 # Terminate already running bar instances
-killall -q 9 polybar
+pkill -9 polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 
 # launch again
-MONITOR=$monitor polybar top >>/tmp/polybar1.log 2>&1 &
-MONITOR=$monitor polybar bottom >>/tmp/polybar1.log 2>&1 &
+MONITOR=$monitor polybar top >> $logfile 2>&1 &
+MONITOR=$monitor polybar bottom >> $logfile 2>&1 &
