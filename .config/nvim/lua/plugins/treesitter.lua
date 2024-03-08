59adef4c448b8d05b7ee9388d8881aef228ef80a
mrdotb <mrdotb@protonmail.com>
2024-03-08 09:38:21 +0100

    remove semanticTokensProvider

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,5 @@
 local ts = require('nvim-treesitter.configs')
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    client.server_capabilities.semanticTokensProvider = nil
-  end,
-});
-
 ts.setup({
   sync_install = false,
   auto_install = true,
